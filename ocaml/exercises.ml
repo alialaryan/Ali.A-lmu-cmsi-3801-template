32689ce1e0af859d6a605f1ad659af1bbd955508
alialaryan <112988088+alialaryan@users.noreply.github.com>
2024-11-04 19:57:41 -0800

    Completed exercises.ml

--- ocaml/exercises.ml
+++ ocaml/exercises.ml
@@ -12,12 +12,87 @@ let change amount =
     in
     aux amount denominations
 
-(* Write your first then apply function here *)
 
-(* Write your powers generator here *)
+ (* Video homework helper *)
+let first_then_apply array predicate consumer=
+  match List.find_opt predicate array with 
+  | None -> None
+  | Some x -> consumer x
 
-(* Write your line count function here *)
 
-(* Write your shape type and associated functions here *)
+(* Video homework helper *)
+let powers_generator base = 
+   let rec generate_from power () = 
+    Seq.Cons (power, generate_from (power * base))
+  in 
+  generate_from 1;;
 
-(* Write your binary search tree implementation here *)
+
+
+    let meaningful_line_count filename =
+      let meaningful_line line =
+        let trimmed = String.trim line in
+        String.length trimmed > 0 && not (String.get trimmed 0 = '#') (* String.starts_with instead *)
+      in
+      let the_file = open_in filename in
+      let finally () = close_in the_file in
+      let rec count_lines count =
+        try
+          let line = input_line the_file in
+          if meaningful_line line then
+            count_lines (count + 1)
+          else
+            count_lines count
+        with
+        | End_of_file -> count
+      in
+      Fun.protect ~finally (fun () -> count_lines 0)
+    
+
+ (* Video homework helper *)
+ type shape =
+ | Sphere of float
+ | Box of float * float * float
+
+let volume s =
+ match s with
+ | Sphere r -> Float.pi *. (r ** 3.) *. 4. /. 3.
+ | Box (l, w, h) -> l *. w *. h
+
+ let surface_area a =
+  match a with
+  | Sphere r -> Float.pi *. 4. *. r *. r
+  | Box (l, w, h) -> 2.0 *. (l *. w +. w *. h +. h *. l)
+
+
+ (* Video homework helper *)
+ type 'a binary_search_tree =
+ | Empty
+ | Node of 'a binary_search_tree * 'a * 'a binary_search_tree
+
+let rec size_helper = function
+ | Empty -> 0
+ | Node (l, _, r) -> 1 + size_helper l + size_helper r
+
+let size tree = size_helper tree
+
+let rec contains_helper v = function
+ | Empty -> false
+ | Node (l, x, r) -> v = x || contains_helper v (if v < x then l else r)
+
+let contains v tree = contains_helper v tree
+
+let rec inorder_helper = function
+ | Empty -> []
+ | Node (l, x, r) -> inorder_helper l @ [x] @ inorder_helper r
+
+let inorder tree = inorder_helper tree
+
+let rec insert_helper v = function
+ | Empty -> Node (Empty, v, Empty)
+ | Node (l, x, r) ->
+   if v < x then Node (insert_helper v l, x, r)
+   else if v > x then Node (l, x, insert_helper v r)
+   else Node (l, x, r)
+
+let insert v tree = insert_helper v tree
